0f711f019d987c9975410bfe84aedb540298e86e
cv <cynosure.veda@gmail.com>
2025-08-09 03:29:44 +1000

    change lib-ui's test1 to use mat-car and apply it in app-main

--- projects/lib-ui/src/lib/test1/test1.ts
+++ projects/lib-ui/src/lib/test1/test1.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'lib-test1',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatMenuModule, MatIconModule],
   template: `
-    <p>
-      test1 works!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    </p>
+    <mat-card class="card1">
+      <mat-card-header>
+        <mat-card-title>
+          {{title}}
+          <button mat-icon-button class="more-button" [matMenuTriggerFor]="menu" aria-label="Toggle menu">
+            <mat-icon>more_vert</mat-icon>
+          </button>
+          <mat-menu #menu="matMenu" xPosition="before">
+            <button mat-menu-item>Expand</button>
+            <button mat-menu-item>Remove</button>
+          </mat-menu>
+        </mat-card-title>
+      </mat-card-header>
+      <mat-card-content>
+        <ng-content></ng-content>
+      </mat-card-content>
+    </mat-card>
   `,
   styles: ``
 })
 export class Test1 {
-
+  @Input() title: string = 'Test1 Card Title';
 }
