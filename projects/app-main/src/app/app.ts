d41445120b0d59371cef645df89c230e48045d8c
cv <cynosure.veda@gmail.com>
2025-08-09 23:45:00 +1000

    update app page

--- projects/app-main/src/app/app.ts
+++ projects/app-main/src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Test1 } from '@geosense-plus/lib-ui';
-import { Test2 } from "@geosense-plus/lib-ui";
+import { Test2 } from "@geosense-plus/lib-ui"; 
+import { Nav1 } from "@geosense-plus/lib-ui";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Test1],
+  imports:  [ RouterOutlet
+            , Test1
+            , Test2
+            , Nav1 ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- projects/lib-ui/src/public-api.ts
+++ projects/lib-ui/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/lib-ui';
 export * from './lib/test1/test1';
 export * from './lib/test2/test2';
+export * from './lib/nav1/nav1';
