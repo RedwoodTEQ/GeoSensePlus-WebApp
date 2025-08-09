e1ae6d1d31d7922dadd2aad8233786e34456a149
cv <cynosure.veda@gmail.com>
2025-08-10 02:38:57 +1000

    tidy up app and dashboard pages

--- projects/app-main/src/app/dashboard/dashboard.ts
+++ projects/app-main/src/app/dashboard/dashboard.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Test1 } from '@geosense-plus/lib-ui';
+import { Test2 } from "@geosense-plus/lib-ui"; 
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [ Test1, Test2 ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
